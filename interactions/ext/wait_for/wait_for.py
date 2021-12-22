4d3b6d5d5474d271e61e141587e9f4ccbfe97b6e
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-12-21 23:52:15 -0500

    docs: Add docstrings and a few more typehints

--- interactions/ext/wait_for/wait_for.py
+++ interactions/ext/wait_for/wait_for.py
@@ -1,6 +1,6 @@
 import asyncio
 from inspect import isawaitable
-from typing import Union, Callable, Optional
+from typing import Union, Callable, Optional, Any, Awaitable, Type
 import types
 
 import interactions
@@ -22,8 +22,15 @@ class ExtendedListener(interactions.api.dispatch.Listener):
 
             futs.remove(fut)
 
-    def add(self, name: str):
+    def add(self, name: str) -> asyncio.Future:
+        """
+        Returns a Future that will resolve whenever the supplied event is dispatched
 
+        :param name: The event to listen for
+        :type name: str
+        :return: A future that will be resolved on the next event dispatch with the data given
+        :rtype: asyncio.Future
+        """
         fut = asyncio.get_event_loop().create_future()
         try:
             futures = self.extra_events[name]
@@ -37,15 +44,31 @@ class ExtendedListener(interactions.api.dispatch.Listener):
 interactions.api.dispatch.Listener = ExtendedListener
 
 
-async def wait_for(self: interactions.Client, name: str,
-                   check: Optional[Callable] = None, timeout: Optional[float] = None):  # TODO work on this next
-    assert isinstance(self.websocket.dispatch, ExtendedListener)
+async def wait_for(bot: interactions.Client, name: str,
+                   check: Optional[Callable[..., Union[bool, Awaitable[bool]]]] = None,
+                   timeout: Optional[float] = None) -> Any:
+    """
+    Wait for an event once, and return the result.
+
+    Unlike event decorators, this is not persistent, and can be used to only proceed in a command once an event happens.
+
+    :param bot: The bot that will receive the event
+    :type bot: interactions.Client
+    :param name: The event to wait for
+    :type name: str
+    :param check: A function or coroutine to call, which should return a truthy value if the data should be returned
+    :type check: Callable
+    :param timeout: How long to wait for the event before raising an error
+    :type timeout: float
+    :return: The value of the dispatched event
+    :rtype: Any
+    """
     while True:
-        fut = self.websocket.dispatch.add(name=name)
+        fut = bot.websocket.dispatch.add(name=name)
         try:
             res: list = await asyncio.wait_for(fut, timeout=timeout)
         except asyncio.TimeoutError:
-            self.websocket.dispatch.extra_events[name].remove(fut)
+            bot.websocket.dispatch.extra_events[name].remove(fut)
             raise
 
         if check:
@@ -65,11 +88,19 @@ async def wait_for(self: interactions.Client, name: str,
         return res
 
 
-# noinspection PyTypeHints
 def setup(bot: Union[
     interactions.Client,
-    # type(interactions.Client)
-]):
+    Type[interactions.Client]
+]) -> None:
+    """
+    Apply hooks to a bot to add ``wait_for`` as a method.
+
+    This function isn't strictly needed, as importing ``wait_for`` directly will do the same thing, but having methods
+    applied to the class can be nice sometimes
+
+    :param bot: The bot instance or class to apply hooks to
+    :type bot:
+    """
     if isinstance(bot, type(interactions.Client)):  # class object
         bot.wait_for = wait_for
 
