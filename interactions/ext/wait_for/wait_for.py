18f2d8ce018e208f9407f91f0aa73e13731b2bd0
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-12-22 23:05:29 -0500

    feat: restore interaction events

--- interactions/ext/wait_for/wait_for.py
+++ interactions/ext/wait_for/wait_for.py
@@ -44,6 +44,29 @@ class ExtendedListener(interactions.api.dispatch.Listener):
 interactions.api.dispatch.Listener = ExtendedListener
 
 
+class ExtendedWebSocket(interactions.api.gateway.WebSocket):
+    def handle_dispatch(self, event: str, data: dict) -> None:
+        super().handle_dispatch(event, data)
+
+        # print(event)
+
+        if event == "INTERACTION_CREATE":
+            # print(data)
+            if "type" not in data:
+                return
+
+            context = self.contextualize(data)
+            self.dispatch.dispatch("on_interaction_create", context)
+
+            name: str = interactions.InteractionType(data["type"]).name
+            name = "on_" + name.lower()
+            print(name)
+            self.dispatch.dispatch(name, context)
+
+
+interactions.api.gateway.WebSocket = ExtendedWebSocket
+
+
 async def wait_for(bot: interactions.Client, name: str,
                    check: Optional[Callable[..., Union[bool, Awaitable[bool]]]] = None,
                    timeout: Optional[float] = None) -> Any:
@@ -88,26 +111,55 @@ async def wait_for(bot: interactions.Client, name: str,
         return res
 
 
-def setup(bot: Union[
-    interactions.Client,
-    Type[interactions.Client]
-]) -> None:
+def _replace_values(old, new):
+    """Change all values on new to the values on old. Useful if neither object has __dict__"""
+    for item in dir(old):  # can't use __dict__, this should take everything
+        value = getattr(old, item)
+
+        if hasattr(value, "__call__") or isinstance(value, property):
+            # Don't need to get callables or properties, that would un-overwrite things
+            continue
+
+        try:
+            new.__setattr__(item, value)
+        except AttributeError:
+            pass
+
+
+def setup(
+        bot: interactions.Client,
+        add_method: bool = False,
+        add_interaction_events: bool = True
+) -> None:
     """
-    Apply hooks to a bot to add ``wait_for`` as a method.
+    Apply hooks to a bot to add additional features
 
-    This function isn't strictly needed, as importing ``wait_for`` directly will do the same thing, but having methods
-    applied to the class can be nice sometimes
+    This function required, as importing alone won't extend the classes
 
-    :param bot: The bot instance or class to apply hooks to
-    :type bot:
+    :param Client bot: The bot instance or class to apply hooks to
+    :param bool add_method: If ``wait_for`` should be attached to the bot
+    :param bool add_interaction_events: Whether to add ``on_message_component``, ``on_application_command``, and other interaction event
     """
-    if isinstance(bot, type(interactions.Client)):  # class object
-        bot.wait_for = wait_for
 
-    elif isinstance(bot, interactions.Client):  # instance
-        bot.wait_for = types.MethodType(wait_for, bot)  # untested
-
-    else:
+    if not isinstance(bot, interactions.Client):
         raise TypeError(f"{bot.__class__.__name__} is not interactions.Client!")
 
-    bot.websocket.dispatch = ExtendedListener()
+    if add_method:
+        bot.wait_for = types.MethodType(wait_for, bot)
+
+    if add_interaction_events:
+        old_websocket = bot.websocket
+        new_websocket = ExtendedWebSocket(old_websocket.intents, old_websocket.session_id, old_websocket.sequence)
+
+        _replace_values(old_websocket, new_websocket)
+
+        bot.websocket = new_websocket
+
+    # Overwrite the listener with the new one
+    new_listener = ExtendedListener()
+    old_listener = bot.websocket.dispatch
+    new_listener.loop = old_listener.loop
+    new_listener.events = old_listener.events
+    bot.websocket.dispatch = new_listener
+
+
