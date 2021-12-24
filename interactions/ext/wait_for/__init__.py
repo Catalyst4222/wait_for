abd1d561d76e805c4892da1b7530b3eaa0131f7d
Catalyst4 <84055084+Catalyst4222@users.noreply.github.com>
2021-12-23 21:16:35 -0500

    chore: update version and setup.py

--- interactions/ext/wait_for/__init__.py
+++ interactions/ext/wait_for/__init__.py
@@ -1,5 +1,5 @@
 from interactions.base import __version__ as __lib_version__
-__version__ = "0.0.1"
+__version__ = "1.0.0"
 __ext_version__ = f"{__lib_version__}:{__version__}"
 
 from .wait_for import *
--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ import re
 with open('interactions/ext/wait_for/__init__.py') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
 
-# with open("README.md", "r", encoding="utf-8") as f:
-#     long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
@@ -14,8 +14,8 @@ setup(
     name="interactions-wait-for",
     version=version,
     description="Add a wait_for function to discord-py-interactions",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/Catalyst4222/interactions-wait-for",
     author="Catalyst4",
     author_email="catalyst4222@gmail.com",
